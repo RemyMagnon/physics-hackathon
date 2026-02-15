08678df67745f74b965741fce1c2d0e6da760185
tri0let <thomas.e.robinson.v@gmail.com>
2026-02-15 11:38:49 -0500

    Increased gravity sound

--- Game/Game.py
+++ Game/Game.py
@@ -14,7 +14,7 @@ pygame.init()
 
 pygame.mixer.init()
 tone = pygame.mixer.Sound("100hz_tone.wav")
-tone.set_volume(0.1)
+tone.set_volume(1)
 channel = None  # We'll use a specific channel to control the sound
 
 #Background music
